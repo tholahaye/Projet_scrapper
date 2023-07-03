420ca26b1ad1caf4d4e3e8c4124f44e01de88555
thomas lahaye <tholahaye@laposte.net>
2023-07-03 17:31:14 +0200

    Ajout de la fonction inserted_urls
    Ajout de la fonction insterted_urls qui vérifie que l'url traité n'est pas déjà dans la collection.
    Modification de la fonction insert_links pour predre en compte le return de inserted_urls.

--- main.py
+++ main.py
@@ -27,16 +27,25 @@ def absolute_links(url, soup):
     return links_with_text
 
 
+#Fonction qui renvoit True si l'url est déjà dans la collection cible, False sinon.
+def inserted_urls(url, collection):
+    for document in collection.find():
+        if document["url_du_lien"] == url:
+            return True
+    return False
+
+
 def insert_links(url, collection, limite=None):
     r = requests.get(url) #Requête HTTP sur la page cible
     soup = bs4.BeautifulSoup(r.content, 'html.parser')#Parsing du code HTML de la page
     decompte = 0
     for link in absolute_links(r.url, soup):
-        collection.insert_one({"url_de_la_page": f"{r.url}", "url_du_lien": f"{link}"})
-        print(f"Bien inséré à la db :{link}")
-        decompte += 1
-        if decompte == limite: #pour s'arrêter à la limite
-            break
+        if not inserted_urls(url, collection):
+            collection.insert_one({"url_de_la_page": f"{r.url}", "url_du_lien": f"{link}"})
+            print(f"Bien inséré à la db :{link}")
+            decompte += 1
+            if decompte == limite: #pour s'arrêter à la limite
+                break
 
 
 insert_links(url, collection_test)
