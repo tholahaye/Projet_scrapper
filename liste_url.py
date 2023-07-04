72393a8a44dcde8dd91c4f736ed3298322bbf463
thomas lahaye <tholahaye@laposte.net>
2023-07-04 12:13:27 +0200

    Fonction check_scope
    Ajout de la fonction qui vérifie que l'url traité est dans le scope.
    Intégration d'un appel de check_scope dans insert_links.

--- liste_url.py
+++ liste_url.py
@@ -2,19 +2,31 @@ import requests
 import bs4
 from urllib.parse import urljoin, urlparse
 
+
 def check_domain(url, list_domains):
     parsed_url = urlparse(url)
     for domain in list_domains:
-        if parsed_url['netloc'].endswith(domain):
+        if parsed_url.netloc.endswith(domain):
             return True
     return False
 
-def check_repositry(url, list_repository):
+
+def check_repository(url, list_repository):
     parsed_url = urlparse(url)
     for repository in list_repository:
-        if parsed_url['path'].startswith(repository):
+        if parsed_url.path.startswith(repository):
             return True
     return False
+
+
+def check_scope(url, list_domains, list_repositories):
+    if check_domain(url, list_domains):
+        if len(list_repositories) == 0:
+            return True
+        if check_repository(url, list_repositories):
+            return True
+    return False
+
 
 def absolute_links(url, soup):
     links_with_text = []
@@ -28,12 +40,6 @@ def absolute_links(url, soup):
     links_with_text = set(links_with_text)
     return links_with_text
 
-def check_scope(url, list_domains):
-    parsed_url = urlparse(url)
-    for domain in list_domains:
-        if parsed_url["netloc"].endswith("domain"):
-            return True
-    return False
 
 #Fonction qui renvoit True si l'url est déjà dans la collection cible, False sinon.
 def inserted_urls(url, collection):
@@ -43,14 +49,17 @@ def inserted_urls(url, collection):
     return False
 
 
-def insert_links(url, collection, limite=None):
+def insert_links(url, collection, list_domains, list_repositories=None, limite=None):
     r = requests.get(url) #Requête HTTP sur la page cible
     soup = bs4.BeautifulSoup(r.content, 'html.parser')#Parsing du code HTML de la page
     decompte = 0
     for link in absolute_links(r.url, soup):
-        if not inserted_urls(url, collection):
-            collection.insert_one({"url_de_la_page": f"{r.url}", "url_du_lien": f"{link}"})
-            print(f"Bien inséré à la db :{link}")
-            decompte += 1
-            if decompte == limite: #pour s'arrêter à la limite
-                break
+        if list_repositories is None:
+            list_repositories = []
+        if check_scope(link, list_domains, list_repositories):
+            if not inserted_urls(url, collection):
+                collection.insert_one({"url_de_la_page": f"{r.url}", "url_du_lien": f"{link}"})
+                print(f"Bien inséré à la db :{link}")
+                decompte += 1
+                if decompte == limite: #pour s'arrêter à la limite
+                    break
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pymongo
 import liste_url
+from urllib.parse import urljoin, urlparse
 
 #Création du client MongoDB
 client = pymongo.MongoClient('localhost')
@@ -7,7 +8,12 @@ client = pymongo.MongoClient('localhost')
 db_test = client.test_scrapping_3
 #Création de la collection
 collection_test = db_test.scrapped_url
+#Définition du scope:
+list_domains = ["fr.wikipedia.org"]
+list_repositories = ["/wiki/"]
 
 url = input("URL : ")
 
+
 liste_url.insert_links(url, collection_test)
+
